1377a13ddde5211705301f2dc0c5bcb5b744132c
Song Huang <song@Mengs-Mac-mini.local>
2021-07-27 22:45:33 -0400

    Directories used for TopN tests

--- jianbing/__init__.py
+++ jianbing/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 from . import hsc
 from . import venn
 from . import split
@@ -14,5 +16,15 @@ from . import catalog
 from . import wlensing
 from . import simulation
 
-__all__ = ["hsc", "venn", "split", "utils", "cutout", "wlensing", "catalog", 
+__all__ = ["hsc", "venn", "split", "utils", "cutout", "wlensing", "catalog",
            "catalog", "profile", "prepare", "visual", "simulation"]
+
+HOME = os.environ['HOME']
+DATA_DIR = os.path.join(HOME, "astro1/hsc/jianbing")
+
+JB_DIR = os.path.join(HOME, "Dropbox/work/submit/jianbing")
+
+SIM_DIR = os.path.join(JB_DIR, "data", "simulation")
+RES_DIR = os.path.join(JB_DIR, "data", "results")
+BIN_DIR = os.path.join(JB_DIR, "data", "bins")
+FIG_DIR = os.path.join(JB_DIR, "paper", "figure")
